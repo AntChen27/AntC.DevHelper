7c503492b2ba04d94abd5dfb25de31067f805ae3
AntChen <1151355296@qq.com>
2021-08-07 10:33:41 +0800

    移除不必要的项目

--- src/CodeGenerate/AntC.CodeGenerate.Lib/CodeGenerateExecutors/BaseDbCodeGenerator.cs
+++
@@ -1,8 +0,0 @@
-﻿using AntC.CodeGenerate.Interfaces;
-
-namespace AntC.CodeGenerate.CodeGenerateExecutors
-{
-    public abstract class BaseDbCodeGenerator : BaseCodeGenerator<DbCodeGenerateContext>, IDbCodeGenerator
-    {
-    }
-}
--- src/CodeGenerate/Plugins/AntC.CodeGenerate.Plugin.LZ/Plugin.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace AntC.CodeGenerate.Plugin.LZ
-{
-    public class Plugin : AbstractPlugin
-    {
-    }
-}
